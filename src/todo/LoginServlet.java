efc555caf5b5f08a8fc02de255ec6f562e5788d3
mutsuoKudo <mutsuo.kudo@hokusys.jp>
2019-03-12 13:23:27 +0900

    コメント追加

--- src/todo/LoginServlet.java
+++ src/todo/LoginServlet.java
@@ -31,7 +31,7 @@ public class LoginServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// 今回はdoPostで処理
+		// 今回はdoPostで処理です
 		doPost(request, response);
 	}
 
